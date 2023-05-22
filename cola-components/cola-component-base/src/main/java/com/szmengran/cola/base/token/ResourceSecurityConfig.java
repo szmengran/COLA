5a4ddfd1acd9ede5c1f17201e8cdb27d68a115e9
李茂源 <android_li@sina.cn>
2023-05-22 17:34:22 +0800

    依赖组件添加

--- cola-components/cola-component-base/src/main/java/com/szmengran/cola/base/token/ResourceSecurityConfig.java
+++ cola-components/cola-component-base/src/main/java/com/szmengran/cola/base/token/ResourceSecurityConfig.java
@@ -4,8 +4,6 @@ import jakarta.annotation.Resource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -24,14 +22,13 @@ public class ResourceSecurityConfig {
     private JwtProperties jwtProperties;
 
     @Bean
-    @Order(Ordered.LOWEST_PRECEDENCE)
     @ConditionalOnMissingBean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withJwkSetUri(jwtProperties.getUrl()).build();
     }
 
     @Bean
-    @Order(Ordered.LOWEST_PRECEDENCE)
+    @ConditionalOnMissingBean
     public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http)
             throws Exception {
         http.authorizeHttpRequests()
