5d9b5df67c633ab91f6701eead8eb1a16d3937f1
maoyuan.li@zhenai.com <maoyuan.li@zhenai.com>
2023-10-11 00:07:28 +0800

    版本更新

--- cola-components/cola-component-base/src/main/java/com/szmengran/cola/base/hikari/HikariPoolConfig.java
+++ cola-components/cola-component-base/src/main/java/com/szmengran/cola/base/hikari/HikariPoolConfig.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,20 +19,21 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
-public class HikariPoolConfig {
+public class HikariPoolConfig implements ApplicationRunner {
+
+    @Resource
+    private DataSource dataSource;
 
     /**
      * BUG FIXED for new version HikariPool can't init
-     * @param dataSource
-     * @Return org.springframework.boot.ApplicationRunner
-     * @Date: 2023/10/8 21:20
+     * @param args
+     * @Return void
+     * @Date: 2023/10/8 21:45
      * @Author: <a href="mailto:android_li@sina.cn">MaoYuan.Li</a>
      */
-    @Bean
-    public ApplicationRunner runner(DataSource dataSource) {
-        return args -> {
-            log.info("hikari pool initializing...");
-            Connection connection = dataSource.getConnection();
-        };
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("hikari pool initializing...");
+        dataSource.getConnection();
     }
 }
